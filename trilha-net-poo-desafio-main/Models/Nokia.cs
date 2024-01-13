7f36434ba214044f7fd295aa71668303bdc612d5
LucasVanLume <lucasvanlume@hotmail.com>
2024-01-13 12:22:24 -0300

    feat(trilha-net-poo-desafio): creating Nokia and iPhone child classes of the Smartphone class

--- trilha-net-poo-desafio-main/Models/Nokia.cs
+++ trilha-net-poo-desafio-main/Models/Nokia.cs
@@ -1,8 +1,15 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+
+        }
+
+        public override void InstalarAplicativo(string app)
+        {
+            Console.WriteLine($"O app {app} está sendo instalado no seu Nokia");
+        }
     }
 }
--- trilha-net-poo-desafio-main/Program.cs
+++ trilha-net-poo-desafio-main/Program.cs
@@ -1,3 +1,19 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "1234", modelo: "XR", imei: "xxx-222", memoria: 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Classroom");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "4321", modelo: "Tijolão", imei: "222-xxx", memoria: 128);
+nokia.ReceberLigacao();
+nokia.Ligar();
+nokia.InstalarAplicativo("Google Chrome");
+
+Console.WriteLine("\n");
